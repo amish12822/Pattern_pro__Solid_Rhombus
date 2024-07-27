1a621a6c6d837be61dffcea5a882b81364a9bea7
amish12822 <amishofficial23@gmail.com>
2024-07-28 01:18:50 +0530

    complete program solid rhombus

---
+++ solid_rhombus.java
@@ -0,0 +1,34 @@
+/**
+ * solid_rhombus
+ */
+public class solid_rhombus {
+    
+    public static void main(String[] args) {
+        
+
+        int a = 5;
+        
+
+        for ( int i=1; i<=a; i++){
+            
+            for ( int j=1; j<=a-i; j++){
+
+                System.out.print(" ");
+            }
+
+
+            for (int j=1; j<=a; j++){
+                System.out.print("* ");
+            }
+
+
+
+
+
+
+
+            System.out.println();
+        }
+    }
+    
+}
