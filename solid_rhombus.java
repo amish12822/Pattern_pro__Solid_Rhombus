4d856069b813d5a1559f6f40aac4619a4d5b63a0
amish12822 <amishofficial23@gmail.com>
2024-07-28 18:29:15 +0530

    add second method to find solid rhombus

--- solid_rhombus.java
+++ solid_rhombus.java
@@ -1,34 +1,50 @@
 /**
  * solid_rhombus
- */
+*/
 public class solid_rhombus {
     
     public static void main(String[] args) {
         
 
-        int a = 5;
-        
+        // First method
 
+
+        int a = 5;
+    
         for ( int i=1; i<=a; i++){
             
             for ( int j=1; j<=a-i; j++){
-
                 System.out.print(" ");
             }
-
 
             for (int j=1; j<=a; j++){
                 System.out.print("* ");
             }
 
+            System.out.println();
+        }
+    
 
 
+        // Second Method
 
+        int n = 5;
 
+        for ( int i=1; i<=5; i++){
 
+            // space
+            int space = ( n-i );
+            for ( int j=1; j<=space; j++){
+                System.out.print(" ");
+            }
+
+            for ( int j=1; j<=5; j++){
+                System.out.print("* ");
+            }
 
             System.out.println();
         }
+       
+        
     }
-    
 }
